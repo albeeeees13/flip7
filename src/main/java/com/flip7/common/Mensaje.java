a0154459410e5c51dfa323a2a9b06924319b4163
KEVIN ALBESTRAIN <albes@MacBook-Air-de-KEVIN.local>
2025-12-08 07:20:15 -0700

    agregue la clase mensaje para el protocolo de red

---
+++ src/main/java/com/flip7/common/Mensaje.java
@@ -0,0 +1,42 @@
+package com.flip7.common;
+
+import java.io.Serializable;
+
+public class Mensaje implements Serializable {
+
+    public enum Tipo {
+        LOGIN,
+        LOGIN_EXITO,
+        ERROR,
+        CREAR_SALA,
+        UNIRSE_SALA,
+        LISTA_SALAS,
+        ROL_ASIGNADO,
+        INICIO_JUEGO,
+        ACCION_SACAR,
+        ACCION_PLANTARSE,
+        ACTUALIZAR_TABLERO,
+        SOLICITAR_OBJETIVO,
+        SELECCIONAR_OBJETIVO,
+        MENSAJE_CHAT,
+        FIN_JUEGO
+    }
+
+    private Tipo tipo;
+    private Object contenido;
+    private String remitente;
+
+    public Mensaje(Tipo tipo, Object contenido, String remitente) {
+        this.tipo = tipo;
+        this.contenido = contenido;
+        this.remitente = remitente;
+    }
+
+    public Mensaje(Tipo tipo, Object contenido) {
+        this(tipo, contenido, "SISTEMA");
+    }
+
+    public Tipo getTipo() { return tipo; }
+    public Object getContenido() { return contenido; }
+    public String getRemitente() { return remitente; }
+}
