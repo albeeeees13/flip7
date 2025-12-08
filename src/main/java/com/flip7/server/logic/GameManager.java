04d2d8219d4f65940726860b7a36cc91ff14e18c
johanarellano33-glitch <johanarellano33@gmail.com>
2025-12-08 13:54:57 -0700

    agregue metodos que nos ayudan al cliente en game manager

--- src/main/java/com/flip7/server/logic/GameManager.java
+++ src/main/java/com/flip7/server/logic/GameManager.java
@@ -17,7 +17,54 @@ public class GameManager {
 
 
     private MotorReglas reglas;
-    private List<Carta> cartasEnMesa; 
+    private List<Carta> cartasEnMesa;
 
 
     private Timer timerTurno;
+    public GameManager(String idSala) {
+        this.idSala = idSala;
+        this.jugadores = new ArrayList<>();
+        this.cartasEnMesa = new ArrayList<>();
+        this.reglas = new MotorReglas(); // SRP: Delegamos la matemática aquí
+    }
+
+    // --- GESTIÓN DE JUGADORES ---
+
+    public void agregarJugador(ClientHandler jugador) {
+        if (jugadores.size() < 2 && !juegoIniciado) {
+            jugadores.add(jugador);
+            // Avisar al jugador que entró
+            jugador.enviarMensaje(new Mensaje(Mensaje.Tipo.ROL_ASIGNADO, "JUGADOR"));
+
+            // Si ya estamos listos (2 jugadores), iniciamos
+            if (jugadores.size() == 2) {
+                iniciarPartida();
+            }
+        } else {
+            // Manejo de Espectador (Extra SOLID: No mezclamos lógica de juego con lógica de espectador)
+            jugador.enviarMensaje(new Mensaje(Mensaje.Tipo.ROL_ASIGNADO, "ESPECTADOR"));
+        }
+    }
+
+    // --- FLUJO DEL JUEGO ---
+
+    private void iniciarPartida() {
+        juegoIniciado = true;
+        broadcast(new Mensaje(Mensaje.Tipo.INICIO_JUEGO, "¡La partida ha comenzado!"));
+        iniciarTurno();
+    }
+
+    private void iniciarTurno() {
+        ClientHandler actual = getJugadorActual();
+        broadcast(new Mensaje(Mensaje.Tipo.ACTUALIZAR_TABLERO, "Turno de: " + actual.getNombreUsuario()));
+
+        // Reiniciar Timer de 60s
+        if (timerTurno != null) timerTurno.cancel();
+        timerTurno = new Timer();
+        timerTurno.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                forzarFinDeTurno(); // Castigo por tiempo
+            }
+        }, 60000);
+    }
