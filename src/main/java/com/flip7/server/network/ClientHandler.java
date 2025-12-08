d7a44e99bdb989bf9372281c59b61ae85f6348a3
johanarellano33-glitch <johanarellano33@gmail.com>
2025-12-08 13:43:56 -0700

    agregue estructura de server network clienthandler y agregue el clienthadler

---
+++ src/main/java/com/flip7/server/network/ClientHandler.java
@@ -0,0 +1,77 @@
+package com.flip7.server.network;
+
+import com.flip7.common.Mensaje;
+import com.flip7.server.logic.GameManager; 
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable {
+
+    private Socket socket;
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
+    private String nombreUsuario;
+    private boolean conectado = true;
+
+
+    private GameManager salaActual;
+
+    public ClientHandler(Socket socket) {
+        this.socket = socket;
+        try {
+
+            this.out = new ObjectOutputStream(socket.getOutputStream());
+            this.in = new ObjectInputStream(socket.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+            conectado = false;
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (conectado) {
+
+                Mensaje mensajeRecibido = (Mensaje) in.readObject();
+
+                System.out.println("Mensaje recibido de " + nombreUsuario + ": " + mensajeRecibido.getTipo());
+
+
+                if (mensajeRecibido.getTipo() == Mensaje.Tipo.LOGIN) {
+                    this.nombreUsuario = (String) mensajeRecibido.getContenido();
+                }
+
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            cerrarConexion();
+        }
+    }
+
+
+    public void enviarMensaje(Mensaje msg) {
+        try {
+            out.writeObject(msg);
+            out.flush();
+        } catch (IOException e) {
+            System.err.println("Error enviando mensaje a " + nombreUsuario);
+            cerrarConexion();
+        }
+    }
+
+    public String getNombreUsuario() {
+        return (nombreUsuario != null) ? nombreUsuario : "Anónimo";
+    }
+
+    private void cerrarConexion() {
+        conectado = false;
+        try {
+            if (socket != null) socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
