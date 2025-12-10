92f44a668d93a1e43ccd7fc5be70368474c5e1fc
joahan david <johanarellano33@gmail.com>
2025-12-10 10:17:01 -0700

    login y resgistar

--- src/main/java/com/flip7/server/data/UsuarioDAO.java
+++ src/main/java/com/flip7/server/data/UsuarioDAO.java
@@ -1,4 +1,38 @@
 package com.flip7.server.data;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class UsuarioDAO {
+
+    public boolean login(String user, String pass) {
+        String sql = "SELECT id FROM usuarios WHERE username = ? AND password = ?";
+        try (Connection conn = DatabaseConnection.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, user);
+            pstmt.setString(2, pass);
+            ResultSet rs = pstmt.executeQuery();
+            return rs.next();
+        } catch (SQLException e) {
+            System.out.println("Error SQL Login: " + e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean registrar(String user, String pass) {
+        String sql = "INSERT INTO usuarios(username, password) VALUES(?,?)";
+        try (Connection conn = DatabaseConnection.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, user);
+            pstmt.setString(2, pass);
+            pstmt.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            return false;
+        }
+    }
 }
