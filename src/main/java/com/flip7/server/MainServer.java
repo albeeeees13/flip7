c9a413776b1fb9c6b9b287cf5130bba82882480c
joahan david <johanarellano33@gmail.com>
2025-12-10 10:37:30 -0700

    modifique el main server para la base de datos

--- src/main/java/com/flip7/server/MainServer.java
+++ src/main/java/com/flip7/server/MainServer.java
@@ -1,9 +1,14 @@
 package com.flip7.server;
 
+import com.flip7.server.data.DatabaseConnection;
 import com.flip7.server.network.ServerManager;
 
 public class MainServer {
+
     public static void main(String[] args) {
+        System.out.println("--- INICIANDO SERVIDOR FLIP 7 ---");
+        
+        DatabaseConnection.inicializarBD();
 
         ServerManager server = new ServerManager();
         server.iniciar();
